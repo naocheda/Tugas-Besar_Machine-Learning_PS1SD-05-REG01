d185416a6c387de42da87119b7f83b39af8896ca
chiacaw <abidjaya2006@gmail.com>
2026-06-28 18:47:12 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,3 @@
-# Referensi PyTorch Dataset & DataLoader: https://pytorch.org/docs/stable/data.html
-# Referensi Torchvision Transforms: https://pytorch.org/vision/stable/transforms.html
-
 import os
 import torch
 import pandas as pd
@@ -11,7 +8,6 @@ from torchvision import transforms
 class AlzheimerDataset(Dataset):
     def __init__(self, csv_file, root_dir, split_type, transform=None):
         df = pd.read_csv(csv_file)
-        # Memfilter baris berdasarkan 'train' atau 'test'
         self.data = df[df['Split'] == split_type].reset_index(drop=True)
         self.root_dir = root_dir
         self.transform = transform
@@ -23,17 +19,14 @@ class AlzheimerDataset(Dataset):
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
         img_path = os.path.join(self.root_dir, row['Split'], row['Disease'], row['Filename'])
-        # MobileNetV2 dari Kaggle membutuhkan format 3-channel RGB
         image = Image.open(img_path).convert("RGB")
         label = self.label_map[row['Disease']]
         
         if self.transform:
             image = self.transform(image)
-            
         return image, label
 
 def get_dataloaders(csv_path, root_dir, batch_size=32):
-    # Menggunakan nilai normalisasi standar ImageNet sesuai kode Kaggle
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(),
